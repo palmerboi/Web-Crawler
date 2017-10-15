f850612c4aaa6e61b92987520a98091802ebdcfe
Alex Alnaib <xwg7735@autuni.ac.nz>
2017-10-15 17:37:33 +1300

    k

--- Assignment 2/Web Crawler/CrawlerGUI.Designer.cs
+++ Assignment 2/Web Crawler/CrawlerGUI.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // CrawlerGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(572, 303);
+            this.Name = "CrawlerGUI";
             this.Text = "CrawlerGUI";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
