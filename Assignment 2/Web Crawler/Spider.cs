bc223fdda84faaa57ad2f2020b87144dadf3453a
Reuben Palmer <reuben.j.palmer@gmail.com>
2017-10-15 16:26:36 +1300

    finished q1, begun q2

---
+++ Assignment 2/Web Crawler/Spider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web_Crawler
+{
+    class Spider
+    {
+
+
+        static void main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Assignment 2/Web Crawler/SpiderLeg.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace Web_Crawler
+{
+    class SpiderLeg
+    {
+        private string url;
+        private HtmlWeb web;
+        private HtmlDocument htmlDoc;
+        List<string> URLs;
+        List<Image> imgList;
+        Metadata metaData;
+
+        public SpiderLeg(string url)
+        {
+            this.url = url;
+            web = new HtmlWeb();
+            try
+            {
+                htmlDoc = web.Load(url);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("You suck");
+            }
+            URLs = new List<string>();
+            metaData = new Metadata();
+            imgList = new List<Image>();
+        }
+
+        //static void Main(string[] args)
+        //{
+        //    SpiderLeg leg = new SpiderLeg("https://www.w3schools.com/tags/tag_meta.asp");
+
+        //    Console.WriteLine(leg.htmlDoc.DocumentNode.OuterHtml);
+        //    Console.WriteLine();
+
+        //    Console.WriteLine(leg.getTitle());
+        //    Console.WriteLine();
+
+        //    leg.getHyperlink();
+        //    foreach(String url in leg.URLs)
+        //    {
+        //        Console.WriteLine(url);
+        //    }
+        //    Console.WriteLine();
+
+        //    leg.getImages();
+        //    foreach(Image img in leg.imgList)
+        //    {
+        //        Console.WriteLine(img.name+" width: "+img.width+" height: "+img.height);
+        //    }
+        //    Console.WriteLine();
+
+        //    leg.getMeta();
+        //    Console.WriteLine("Description: "+leg.metaData.description);
+        //    foreach(string keyword in leg.metaData.keywords)
+        //    {
+        //        Console.WriteLine(keyword);
+        //    }
+        //}
+
+        public string getTitle()
+        {
+            var node = htmlDoc.DocumentNode.SelectSingleNode("//head/title");
+            var title = node.InnerHtml;
+            return title;
+        }
+
+        public void getHyperlink()
+        {
+            foreach (HtmlNode link in htmlDoc.DocumentNode.SelectNodes("//a"))
+            {
+                // Get the link text in the href attribute
+                string hrefValue = link.GetAttributeValue("href", string.Empty);
+                if (hrefValue.Contains("http")) // make sure its a URL
+                {
+                    URLs.Add(hrefValue);
+                }
+            }
+        }
+
+        public void getImages()
+        {
+            foreach (HtmlNode img in htmlDoc.DocumentNode.SelectNodes("//img[@src]"))
+            {
+                string imgSource = img.GetAttributeValue("src", string.Empty);
+                string imgWidth = img.GetAttributeValue("width", string.Empty);
+                string imgHeight = img.GetAttributeValue("height", string.Empty);
+                Image image = new Image { name = imgSource, width = imgWidth, height = imgHeight};
+                imgList.Add(image);
+            }
+        }
+
+        public void getMeta()
+        {
+            string description = string.Empty;
+            string keywords = string.Empty;
+
+            foreach (HtmlNode meta in htmlDoc.DocumentNode.SelectNodes("//meta"))
+            {
+                if (meta.GetAttributeValue("name", string.Empty).ToLower().Equals("description"))
+                {
+                    description = meta.GetAttributeValue("name", string.Empty);
+                }
+                if (meta.GetAttributeValue("name", string.Empty).ToLower().Equals("keywords"))
+                {
+                    keywords = meta.GetAttributeValue("content", string.Empty);
+                }
+            }
+
+            metaData.description = description;
+            string[] keywordsarray = keywords.Split(',');
+            foreach(string keyword in keywordsarray)
+            {
+                metaData.keywords.Add(keyword);
+            }
+        }
+
+        //represents an image file from a html document
+        public class Image
+        {
+            public string name { get; set; }
+            public string width { get; set; }
+            public string height { get; set; }
+        }
+
+        //represents the metadata from a html document
+        public class Metadata
+        {
+            public string description;
+            public List<string> keywords;
+
+            public Metadata()
+            {
+                keywords = new List<string>();
+            }
+        }
+    }
+}
